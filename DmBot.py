83a500446baf6eb36e05cc642a0da66b1b5abc12
Hugo <hugo.bollon@gmail.com>
2020-10-06 18:21:16 +0200

    Added config attributes type validation

--- DmBot.py
+++ DmBot.py
@@ -14,18 +14,18 @@ class Config(object):
     config = ConfuseConfig('IG_Automation_Bot', __name__)
 
     def __init__(self):
-        self.app_name = self.config['app_name'].get()
-        self.username = self.config["account"]["username"].get()
-        self.password = self.config["account"]["password"].get()
-        self.srcAccount = self.config["users_src"]["src_account"].get()
-        self.fetchQuantity = self.config["users_src"]["fetch_quantity"].get()
-        self.autoFollow = self.config["bot"]["auto_follow"].get()
-        self.autoDm = self.config["bot"]["auto_dm"].get()
-        self.maxDmQuantity = self.config["bot"]["max_dm_quantity"].get()
-        self.maxFollowQuantity = self.config["bot"]["max_follow_quantity"].get()
-        self.blacklistInteractedUsers = self.config["bot"]["blacklist_interacted_users"].get()
-        self.headlessBrowser = self.config["bot"]["headless_browser"].get()
-        self.dmTemplates = self.config["dm_templates"].get()
+        self.app_name = self.config['app_name'].get(str)
+        self.username = self.config["account"]["username"].get(str)
+        self.password = self.config["account"]["password"].get(str)
+        self.srcAccount = self.config["users_src"]["src_account"].get(str)
+        self.fetchQuantity = self.config["users_src"]["fetch_quantity"].get(int)
+        self.autoFollow = self.config["bot"]["auto_follow"].get(bool)
+        self.autoDm = self.config["bot"]["auto_dm"].get(bool)
+        self.maxDmQuantity = self.config["bot"]["max_dm_quantity"].get(int)
+        self.maxFollowQuantity = self.config["bot"]["max_follow_quantity"].get(int)
+        self.blacklistInteractedUsers = self.config["bot"]["blacklist_interacted_users"].get(bool)
+        self.headlessBrowser = self.config["bot"]["headless_browser"].get(bool)
+        self.dmTemplates = self.config["dm_templates"].get(list)
         
 
 def extractUsers():
