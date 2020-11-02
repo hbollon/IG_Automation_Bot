97d21b634bc4f0843a260be8600d6a53227fecb7
Hugo <hugo.bollon@gmail.com>
2020-11-02 18:20:46 +0100

    Added random dm template choice

--- DmBot.py
+++ DmBot.py
@@ -183,7 +183,7 @@ if __name__ == '__main__':
     if(config.autoDm == True):
         for user in followers:
             if usersBlacklist.isBlacklisted(user) == False:
-                messageSend = insta.sendMessage(user=user, message=config.dmTemplates[0], greeting="Salut")
+                messageSend = insta.sendMessage(user=user, message=random.choice(config.dmTemplates) , greeting="Salut")
                 if messageSend:
                     print("Dm sent to "+user)
                     usersBlacklist.addUser(user)
